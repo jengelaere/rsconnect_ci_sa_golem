1ef706c58a9fe860fa2abaf9473980b9385f7bba
Juliette Engelaere-Lefebvre <juliette.engelaere@developpement-durable.gouv.fr>
2023-07-26 18:14:02 +0200

    init :baby:

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# rsconnect.ci.sa.golem 0.0.0.9000
+
+* Added a `NEWS.md` file to track changes to the package.
---
+++ dev/tools/deploy.R
@@ -0,0 +1,34 @@
+message("--- Add server ---\n")
+
+options("repos" = "https://packagemanager.posit.co/cran/__linux__/jammy/latest")
+
+install.packages("remotes")
+install.packages("pak")
+remotes::install_deps()
+remotes::install_local(force = TRUE)
+
+
+message("--- Add server ---\n")
+
+rsconnect::setAccountInfo(
+  name = Sys.getenv("SHINYAPPS_NAME"),
+  token = Sys.getenv("SHINYAPPS_TOKEN"),
+  secret = Sys.getenv("SHINYAPPS_SECRET")
+)
+
+message("--- Deploying app ---\n")
+appFiles <- list.files(".", recursive = TRUE)
+appFiles <- appFiles[!grepl(".Rprofile|renv|rstudio_|dev|data-raw|docker|testthat|test.convert.dsfr", appFiles)]
+
+options(rsconnect.packrat = TRUE)
+# options(rsconnect.http.trace = TRUE)
+options(rsconnect.check.certificate = FALSE)
+
+rsconnect::deployApp(
+  appDir = ".",
+  account = Sys.getenv("SHINYAPPS_NAME"),
+  appFiles = appFiles,
+  appName = Sys.getenv("NAME_OF_APP"),
+  lint = FALSE,
+  forceUpdate = TRUE
+)
